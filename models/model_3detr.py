Request to fix training issues (memory leakage and backward error) (#21)
* fix training memory leakage

* fix training backprop

models/model_3detr.py
@@ -905,8 +905,8 @@ class Model3DETRPredictedBoxDistillationHead(nn.Module):
                                          test=False,
                                          curr_epoch=-1):  # the max number of classes are 18(train) and 14(val)
         gt_center_ori = outputs[
-            "box_corners_xyz"]  # .clone().detach() #inputs["gt_box_corners_xyz"]  # shape: (8, 64, 8, 3) box_predictions["outputs"]['center_unnormalized']
-        objectness_prob = outputs["objectness_prob"]
+            "box_corners_xyz"].detach().clone()  # .clone().detach() #inputs["gt_box_corners_xyz"]  # shape: (8, 64, 8, 3) box_predictions["outputs"]['center_unnormalized']
+        objectness_prob = outputs["objectness_prob"].detach().clone()
         # img_batch_name = inputs['im_name']
         # gt_angles = inputs["gt_box_angles"]
         # gt_sizes = inputs["gt_box_sizes"]
@@ -1217,7 +1217,7 @@ class Model3DETRPredictedBoxDistillationHead(nn.Module):
                                                            curr_epoch=-1,
                                                            if_test=False):  # the max number of classes are 18(train) and 14(val)
         gt_center_ori = outputs[
-            "box_corners_xyz"]  # .clone().detach() #inputs["gt_box_corners_xyz"]  # shape: (8, 64, 8, 3) box_predictions["outputs"]['center_unnormalized']
+            "box_corners_xyz"].detach().clone()  # .clone().detach() #inputs["gt_box_corners_xyz"]  # shape: (8, 64, 8, 3) box_predictions["outputs"]['center_unnormalized']
         objectness_prob = outputs["objectness_prob"].detach().clone()
         K_tensor = inputs['K']
         Rtilt_tensor = inputs['Rtilt']
@@ -3092,7 +3092,7 @@ class Model3DETRMultiClassHead(nn.Module):
                                                            curr_epoch=-1,
                                                            if_test=False):  # the max number of classes are 18(train) and 14(val)
         gt_center_ori = outputs[
-            "box_corners_xyz"]  # .clone().detach() #inputs["gt_box_corners_xyz"]  # shape: (8, 64, 8, 3) box_predictions["outputs"]['center_unnormalized']
+            "box_corners_xyz"].detach().clone()  # .clone().detach() #inputs["gt_box_corners_xyz"]  # shape: (8, 64, 8, 3) box_predictions["outputs"]['center_unnormalized']
         objectness_prob = outputs["objectness_prob"].detach().clone()
         # img_batch_name = inputs['im_name']
         # gt_angles = inputs["gt_box_angles"]

models/transformer.py
@@ -292,28 +292,28 @@ class TransformerEncoderEveryLayer(nn.Module):
         if self.use_ffn:
             # Implementation of Feedforward model
             self.linear1 = nn.Linear(d_model, dim_feedforward, bias=ffn_use_bias)
-            self.dropout = nn.Dropout(dropout, inplace=True)
+            self.dropout = nn.Dropout(dropout, inplace=False)
             self.linear2 = nn.Linear(dim_feedforward, d_model, bias=ffn_use_bias)
             self.norm1 = NORM_DICT[norm_name](d_model)
             self.norm2 = NORM_DICT[norm_name](d_model)
-            self.dropout2 = nn.Dropout(dropout, inplace=True)
+            self.dropout2 = nn.Dropout(dropout, inplace=False)
 
         if self.use_ffn:
             # Implementation of Feedforward model
             self.cross_linear1 = nn.Linear(d_model, dim_feedforward, bias=ffn_use_bias)
-            self.cross_dropout = nn.Dropout(dropout, inplace=True)
+            self.cross_dropout = nn.Dropout(dropout, inplace=False)
             self.cross_linear2 = nn.Linear(dim_feedforward, d_model, bias=ffn_use_bias)
             self.cross_norm1 = NORM_DICT[norm_name](d_model)
             self.cross_norm2 = NORM_DICT[norm_name](d_model)
-            self.cross_dropout2 = nn.Dropout(dropout, inplace=True)
+            self.cross_dropout2 = nn.Dropout(dropout, inplace=False)
 
         # self.norm1 = NORM_DICT[norm_name](d_model)
-        self.dropout1 = nn.Dropout(dropout, inplace=True)
+        self.dropout1 = nn.Dropout(dropout, inplace=False)
 
         self.activation = ACTIVATION_DICT[activation]()
 
         # self.cross_norm1 = NORM_DICT[norm_name](d_model)
-        self.cross_dropout1 = nn.Dropout(dropout, inplace=True)
+        self.cross_dropout1 = nn.Dropout(dropout, inplace=False)
 
         self.cross_activation = ACTIVATION_DICT[activation]()
 
@@ -424,14 +424,14 @@ class TransformerEncoderLayer(nn.Module):
         if self.use_ffn:
             # Implementation of Feedforward model
             self.linear1 = nn.Linear(d_model, dim_feedforward, bias=ffn_use_bias)
-            self.dropout = nn.Dropout(dropout, inplace=True)
+            self.dropout = nn.Dropout(dropout, inplace=False)
             self.linear2 = nn.Linear(dim_feedforward, d_model, bias=ffn_use_bias)
             self.norm2 = NORM_DICT[norm_name](d_model)
             self.norm2 = NORM_DICT[norm_name](d_model)
-            self.dropout2 = nn.Dropout(dropout, inplace=True)
+            self.dropout2 = nn.Dropout(dropout, inplace=False)
 
         self.norm1 = NORM_DICT[norm_name](d_model)
-        self.dropout1 = nn.Dropout(dropout, inplace=True)
+        self.dropout1 = nn.Dropout(dropout, inplace=False)
 
         self.activation = ACTIVATION_DICT[activation]()
         self.normalize_before = normalize_before
@@ -510,13 +510,13 @@ class TransformerDecoderLayer(nn.Module):
         self.norm2 = NORM_DICT[norm_fn_name](d_model)
 
         self.norm3 = NORM_DICT[norm_fn_name](d_model)
-        self.dropout1 = nn.Dropout(dropout, inplace=True)
-        self.dropout2 = nn.Dropout(dropout, inplace=True)
-        self.dropout3 = nn.Dropout(dropout, inplace=True)
+        self.dropout1 = nn.Dropout(dropout, inplace=False)
+        self.dropout2 = nn.Dropout(dropout, inplace=False)
+        self.dropout3 = nn.Dropout(dropout, inplace=False)
 
         # Implementation of Feedforward model
         self.linear1 = nn.Linear(d_model, dim_feedforward)
-        self.dropout = nn.Dropout(dropout, inplace=True)
+        self.dropout = nn.Dropout(dropout, inplace=False)
         self.linear2 = nn.Linear(dim_feedforward, d_model)
 
         self.activation = ACTIVATION_DICT[activation]()
@@ -666,17 +666,17 @@ class TransformerSharedAttentionDecoderLayer(nn.Module):
 
         self.norm3 = NORM_DICT[norm_fn_name](d_model)
         self.norm3_im = NORM_DICT[norm_fn_name](d_model)
-        self.dropout1 = nn.Dropout(dropout, inplace=True)
-        self.dropout2 = nn.Dropout(dropout, inplace=True)
-        self.dropout2_im = nn.Dropout(dropout, inplace=True)
-        self.dropout3 = nn.Dropout(dropout, inplace=True)
-        self.dropout3_im = nn.Dropout(dropout, inplace=True)
+        self.dropout1 = nn.Dropout(dropout, inplace=False)
+        self.dropout2 = nn.Dropout(dropout, inplace=False)
+        self.dropout2_im = nn.Dropout(dropout, inplace=False)
+        self.dropout3 = nn.Dropout(dropout, inplace=False)
+        self.dropout3_im = nn.Dropout(dropout, inplace=False)
 
         # Implementation of Feedforward model
         self.linear1 = nn.Linear(d_model, dim_feedforward)
         self.linear1_im = nn.Linear(d_model, dim_feedforward)
-        self.dropout = nn.Dropout(dropout, inplace=True)
-        self.dropout_im = nn.Dropout(dropout, inplace=True)
+        self.dropout = nn.Dropout(dropout, inplace=False)
+        self.dropout_im = nn.Dropout(dropout, inplace=False)
         self.linear2 = nn.Linear(dim_feedforward, d_model)
         self.linear2_im = nn.Linear(dim_feedforward, d_model)
 
@@ -937,18 +937,18 @@ class TransformerGuidenceSharedAttentionDecoderLayer(nn.Module):
 
         self.norm3 = NORM_DICT[norm_fn_name](d_model)
         self.norm3_im = NORM_DICT[norm_fn_name](d_model)
-        self.dropout1 = nn.Dropout(dropout, inplace=True)
-        self.dropout1_im = nn.Dropout(dropout, inplace=True)
-        self.dropout2 = nn.Dropout(dropout, inplace=True)
-        self.dropout2_im = nn.Dropout(dropout, inplace=True)
-        self.dropout3 = nn.Dropout(dropout, inplace=True)
-        self.dropout3_im = nn.Dropout(dropout, inplace=True)
+        self.dropout1 = nn.Dropout(dropout, inplace=False)
+        self.dropout1_im = nn.Dropout(dropout, inplace=False)
+        self.dropout2 = nn.Dropout(dropout, inplace=False)
+        self.dropout2_im = nn.Dropout(dropout, inplace=False)
+        self.dropout3 = nn.Dropout(dropout, inplace=False)
+        self.dropout3_im = nn.Dropout(dropout, inplace=False)
 
         # Implementation of Feedforward model
         self.linear1 = nn.Linear(d_model, dim_feedforward)
         self.linear1_im = nn.Linear(d_model, dim_feedforward)
-        self.dropout = nn.Dropout(dropout, inplace=True)
-        self.dropout_im = nn.Dropout(dropout, inplace=True)
+        self.dropout = nn.Dropout(dropout, inplace=False)
+        self.dropout_im = nn.Dropout(dropout, inplace=False)
         self.linear2 = nn.Linear(dim_feedforward, d_model)
         self.linear2_im = nn.Linear(dim_feedforward, d_model)
 